fb8b855da2ed57504ae27886e99020e3622397da
Shuaib Sajid <shusajid@gmail.com>
2018-08-12 20:19:32 -0500

    Fixed indenting, will ask Kevin to check

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -10,15 +10,28 @@ render(){
         <h2>Sign In to Get Started</h2>
         <div className="form-group">
           <label htmlFor="email">Email address</label>
-          <input type="email" className="form-control" id="in-email" placeholder="Email" />
+          <input type="email"
+                 className="form-control" 
+                 id="in-email" 
+                 placeholder="Email" />
         </div>
         <div className="form-group">
           <label htmlFor="in-password">Password</label>
-          <input type="password" className="form-control" id="in-password" placeholder="Password"/>
+          <input type="password" 
+                  className="form-control" 
+                  id="in-password" 
+                  placeholder="Password"/>
         </div>
-        <button id="login-btn" type="submit" className="btn btn-default btn-sub">Sign in</button>
+        <button id="login-btn" 
+                type="submit" 
+                className="btn btn-default btn-sub">
+                Sign in
+        </button>
         <p className="sign-up-offer">First time with the app?
-          <span id="sign-up-show" className="fakelink"> Sign up</span>
+          <span id="sign-up-show" 
+                className="fakelink"> 
+                Sign up
+          </span>
         </p>
       </div>
         <div className="about-us">
