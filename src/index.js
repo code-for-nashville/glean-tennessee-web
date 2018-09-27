4bd7e2010ddbc3c9689556399017210afeb66d60
Corey <coreyarice@gmail.com>
2018-09-27 10:45:02 -0500

    Add simple router

--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import history from './navigation/history';
+import router from './navigation/router';
+import routes from './navigation/routes';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const container = document.getElementById('root')
+
+const renderComponent = (component) => {
+  ReactDOM.render(component, container)
+}
+
+const render = (location) => {
+  router.resolve(routes, location)
+    .then(renderComponent)
+    .catch(error => router.resolve(routes, { ...location, error }))
+    .then(renderComponent)
+}
+
+render(history.location) // render the current URL
+history.listen(render)
+
 registerServiceWorker()
---
+++ src/navigation/routes.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Home from '../screens/home'
+import Login from '../screens/login'
+import SignUp from '../screens/signup'
+import Dashboard from '../screens/dashboard'
+import FourOhFour from '../screens/four-oh-four'
+
+const routes = [
+  { path: '/', action: ({ params }) => <Home params={params} /> },
+  { path: '/signup', action: ({ params }) => <SignUp params={params} /> },
+  { path: '/login', action: ({ params }) => <Login params={params} /> },
+  { path: '/dashboard', action: ({ params }) => <Dashboard params={params} /> },
+  { path: '/error', action: ({ params }) => <FourOhFour params={params} /> },
+];
+
+export default routes
