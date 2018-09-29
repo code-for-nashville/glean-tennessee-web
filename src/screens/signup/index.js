e8ca55bfecf8343a94aeceb8090341edd3d54f04
Emily Lemmon <eclaire.lemmon@gmail.com>
2018-09-29 10:57:06 -0500

    convert html to jsx terms

--- src/screens/signup/index.js
+++ src/screens/signup/index.js
@@ -1,51 +1,106 @@
 import React, { Component } from 'react'
 
 export default class SignUp extends Component {
+  state = {}
+
+  onInputChange(e) {
+    const { value, name } = e.target
+    this.setState({ name: value })
+  }
+
+  onSubmit = e => {
+    e.preventDefault()
+  }
+
   render() {
     return (
-      <div class="row">
-        <div class="col-md">
-          <div class="well">
-            <h2>Let's Register</h2>
-            <h5>This is the information SoSA will use to get in touch with you.</h5>
-            <form id="registrationForm">
-              <div class="form-group">
-                <label for="name">Name</label>
-                <input type="text" class="form-control" id="name" placeholder="Name" />
+      <div className="row">
+        <div className="col-md">
+          <div className="well">
+            <h2>Register as a new user</h2>
+            <h5>
+              This is the information SoSA will use to get in touch with you.
+            </h5>
+            <form id="registrationForm" onSubmit={this.onSubmit}>
+              <div className="form-group">
+                <label htmlFor="name">Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="name"
+                  placeholder="Name"
+                />
               </div>
-              <div class="form-group">
-                <label for="up-email">Email address</label>
-                <input type="email" class="form-control" id="up-email" placeholder="Email" />
+              <div className="form-group">
+                <label htmlFor="up-email">Email address</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="up-email"
+                  placeholder="Email"
+                />
               </div>
-              <div class="form-group">
-                <label for="up-password">Password</label>
-                <input type="password" class="form-control" id="up-password" placeholder="Password" />
+              <div className="form-group">
+                <label htmlFor="up-password">Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="up-password"
+                  placeholder="Password"
+                />
               </div>
-              <div class="form-group">
-                <label for="phone">Phone number</label>
-                <input type="tel" class="form-control" id="phone" placeholder="(615) 555-5555" />
+              <div className="form-group">
+                <label htmlFor="phone">Phone number</label>
+                <input
+                  type="tel"
+                  className="form-control"
+                  id="phone"
+                  placeholder="(615) 555-5555"
+                />
               </div>
-              <div class="form-group">
-                <label for="street">Street Address</label>
-                <input type="text" class="form-control" id="street" placeholder="123 Main Street" />
+              <div className="form-group">
+                <label htmlFor="street">Street Address</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="street"
+                  placeholder="123 Main Street"
+                />
               </div>
-              <div class="form-group">
-                <label for="city">City</label>
-                <input type="text" class="form-control" id="city" placeholder="Nashville" />
+              <div className="form-group">
+                <label htmlFor="city">City</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="city"
+                  placeholder="Nashville"
+                />
               </div>
-              <div class="form-group">
-                <label for="state">State</label>
-                <input type="text" class="form-control" id="state" placeholder="Tennessee" />
+              <div className="form-group">
+                <label htmlFor="state">State</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="state"
+                  placeholder="Tennessee"
+                />
               </div>
-              <div class="form-group">
-                <label for="zip">Zip Code</label>
-                <input type="number" class="form-control" id="zip" placeholder="37211" />
+              <div className="form-group">
+                <label htmlFor="zip">Zip Code</label>
+                <input
+                  type="number"
+                  className="form-control"
+                  id="zip"
+                  placeholder="37211"
+                />
               </div>
             </form>
-            <button id="register-btn" type="" class="btn btn-default">Submit</button>
+            <button id="register-btn" type="submit" className="btn btn-default">
+              Submit
+            </button>
           </div>
         </div>
-      </div>
+      </div >
     )
   }
 }
