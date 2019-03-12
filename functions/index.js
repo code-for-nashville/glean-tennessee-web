f157e97d301bc8494a219182e9c17049e02e2751
Corey <coreyarice@gmail.com>
2019-03-11 22:37:57 -0500

    Update template

--- functions/index.js
+++ functions/index.js
@@ -9,16 +9,15 @@ SendGrid.setApiKey(Functions.config().sendgrid.key)
 exports.sendMessage = Functions.https.onCall((data, context) => {
   const name = context.auth.token.name || null;
   const email = context.auth.token.email || null;
-  const {details, subject, phone, address} = data
+  const {details: message, subject, phone, address} = data
   const emailConfig = {
     to: 'sosa.glean.tn@gmail.com',
     from: 'sosa.glean.tn@gmail.com',
     subject,
     templateId: 'd-b1f6d9ac6715453d9c7ba5b3edfbe6a2',
-    substitutionWrappers: ['{{', '}}'],
-    substitutions: {
-      name: name,
-      message: details, 
+    dynamic_template_data: {
+      name,
+      message, 
       phone,
       address,
       email,
